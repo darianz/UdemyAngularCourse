45156af3ce4c48d37a00c4eaae2a86363539a11a
ros stolero <ronst@ac.sce.ac.il>
2019-03-04 18:26:36 +0200

    S3L54

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,7 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('TestRecape','this is a recipe Test','https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto,w_600,h_750,c_fit,fl_strip_profile/https://s3.amazonaws.com/pixtruder/original_images/9bbf07cb31d5ed6f536454f6792d96f16c7bc26b' ),
+    new Recipe('TestRecape', 'this is a recipe Test', '' ),
     new Recipe('TestRecape', 'this is a recipe Test', 'pita' )
 
   ] ;
